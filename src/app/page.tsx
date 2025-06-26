ea5abb0ee2f7cef82774f61d03faf9fb852ba3b7
kaylaine rodrigues <kaylainecosta.01@gmail.com>
2025-06-26 18:22:28 -0300

    tela de login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,80 @@
-import Image from "next/image";
-
-export default function Home() {
+export default function LoginPage() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <div className="relative min-h-screen font-[var(--font-poppins)] text-neutral-900">
+      {/* Logo no topo */}
+      <div className="absolute top-0 left-9 z-10 flex flex-col gap-2 p-8">
+        <img src="/logo.png" alt="Logo Kadoo" className="w-40 h-auto" />
+        <p className="text-xl font-semi">Education</p>
+      </div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+      {/* Container do login */}
+      <div className="flex h-screen items-center justify-center">
+      {/* Imagens de fundo */}
+      <div className="absolute inset-0 -z-10">
+            <img src="/vector.png" alt="vector" className="absolute top-[20%] left-[5%]" />
+            <img src="/vector1.png" className="absolute top-[10%] left-[30%] w-[10%]" />
+            <img src="/vector2.png" className="absolute top-[49%] left-[32%] w-[60px]" />
+            <img src="/vector3.png" className="absolute bottom-[5%] right-[20%]" />
+            <img src="/vector4.png" className="absolute bottom-[5%] right-[6%]" />
+            <img src="/vector5.png" className="absolute top-[60%] right-[6%] -translate-y-1/2" />
+            <img src="/vector6.png" className="absolute top-0 right-0" />
+            <img src="/group4.png" className="absolute top-0 left-[30%]" />
+            <img src="/group6.png" className="absolute bottom-0 left-0 w-[20%]" />
+            <img src="/group7.png" className="absolute bottom-0 left-[5%] w-[25%]" />
+            <img src="/group8.png" className="absolute bottom-0 left-[45%]" />
+            <img src="/group9.png" className="absolute bottom-[10%] left-[45%]" />
+      </div>
+      {/* Parte esquerda */}
+        <div className="w-1/2 flex flex-col text-left px-20 relative overflow-hidden">
+          <h1 className="text-8xl font-bold mt-16">Bem vindo!</h1>
+          <div className="w-[18%] h-[2px] bg-black my-8" />
+          <p className="text-xl mb-6">
+            Você ainda não conhece a Kadoo?
+            <br />
+            Venha conhecer agora!
+          </p>
+          <button className="bg-[#5f2eea] text-white px-5 py-2 rounded-lg w-[20%]">
+            Leia mais!
+          </button>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+
+        {/* Parte direita */}
+        <div className="w-[500px] bg-gray-100 rounded-2xl p-15 shadow-inner mx-auto min-h-[600px]">
+          <h2 className="text-2xl text-center mb-20">Faça seu login</h2>
+          <form className="flex flex-col gap-10">
+            <div>
+              <label className="block mb-1 text-sm font-medium">Email</label>
+              <input
+                type="email"
+                placeholder="Escreva aqui"
+                className="w-full p-3 border border-gray-700 rounded-lg text-sm text-gray-700 bg-white"
+              />
+            </div>
+
+            <div>
+              <label className="block mb-1 text-sm font-medium">Senha</label>
+              <input
+                type="password"
+                placeholder="Escreva aqui"
+                className="w-full p-3 border border-gray-700 rounded-lg text-sm text-gray-700 bg-white"
+              />
+            </div>
+
+            <button
+              type="submit"
+              className="mt-1 bg-gradient-to-r from-[#5f2eea] to-[#5f2eea] text-white rounded-lg px-6 py-3"
+            >
+              Login
+            </button>
+          </form>
+          <p className="text-center mt-6 text-sm">
+            Ainda não possui uma conta?{" "}
+            <a href="#" className="text-[#5f2eea] hover:underline">
+              Cadastre-se!
+            </a>
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
